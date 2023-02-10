db889331bc060cc619cd0d447d585c3bd0f72d45
kimhw7 <khw786678@gmail.com>
2023-02-11 01:12:17 +0900

    ts core types

---
+++ Typescript/section2/app.js
@@ -0,0 +1,5 @@
+var person = {
+    name: "khw",
+    age: 23
+};
+console.log(person.name);
---
+++ Typescript/section2/app.ts
@@ -0,0 +1,11 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: "khw",
+  age: 23,
+  hobbies: ["Sports", "Cooking"],
+};
+
+console.log(person.name);
